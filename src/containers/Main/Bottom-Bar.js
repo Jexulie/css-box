d771c14f42152b77aba128ab25764f67612a9685
Jexulie <fejitj3n@yahoo.com>
2018-07-17 22:09:40 +0300

    the plan

---
+++ src/containers/Main/Bottom-Bar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div className="Bottom-Bar">
+
+        </div>
+    )
+}
+
+/*
+----- app ----(Main)-----Topbar(donno yet)
+        |       |
+        |       |----->--Scene(sandbox)
+        |       |
+        |       |-----<--bottom bar(add-close css settings)
+        |
+        |
+        |---Settings
+                |---------(Css-Settings)
+                |               |--- Sliders(add numbers too)
+                |
+                |--------- ????
+*/
